9d2738e3f0655979861c002a34b2993a2db0d7c1
Madmin27 <admin@nizip.com>
2024-04-28 13:07:01 +0300

    Create gasprun.sh

---
+++ gasprun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOG_FILE="/root/gasp.log"
+echo "[$(date)]: Script çalıştırılıyor..." >> "$LOG_FILE"
+cd /root/avs-operator-setup && ./run.sh opt-in >> "$LOG_FILE" 2>&1
+cd
+
+#chmod +x /root/gasprun.sh
+#* * * * * /root/gasprun.sh
